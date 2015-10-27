02c5420202a5fd6b182d0f366761d2206328c22a
Bohdan Tarlupa <Bohdan.Tarlupa@eleks-software.local>
2015-10-27 15:08:04 +0200

    first supersimple iteration of comments for posts

--- server/publish.js
+++ server/publish.js
@@ -8,6 +8,10 @@ Meteor.publish("singlePost", function(postId) {
   return Posts.find({_id:postId});
 });
 
+Meteor.publish("postComments", function(postId) {
+  return Comments.find({postId:postId});
+});
+
 /* Admin */
 Meteor.publish("listUsers", function(){
   return Meteor.users.find();
